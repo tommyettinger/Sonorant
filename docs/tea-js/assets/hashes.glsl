f3179be2684f1423e843bed6e8489adc06853a0c
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-10 21:57:30 -0700

    Rebuild TeaVM looping demos.

--- docs/tea-js/assets/hashes.glsl
+++ docs/tea-js/assets/hashes.glsl
@@ -1,6 +1,6 @@
 // Just hash functions that can be copied into other files.
-// These use no bit math, so they work on GLSL ES 2.0.
-// Each hash takes an arbitrary-range vec2 and produces a float or floats between 0 inclusive and 1 exclusive.
+// The floo hashes use no bit math, so they work on GLSL ES 2.0.
+// Each floo hash takes an arbitrary-range vec2 and produces a float or floats between 0 inclusive and 1 exclusive.
 // All mantissa bits for each of these hashes seem quite random.
 // About 24 decent-quality random bits can be obtained from any of these hashes if the inputs are not too close.
 
@@ -42,4 +42,38 @@ vec4 floohash42(vec2 p2)
     exp(fract(vec4(0.8566748838545029, 0.7338918566271260, 0.6287067210378087, 0.5385972572236101) * p2.x)
     -fract(vec4(0.8812714616335696, 0.7766393890897682, 0.6844301295853426, 0.6031687406857282) * p2.y))
     ));
+}
+
+// The triffid hashes work on int or ivec input and return one 32-bit int, all bits of which should be random.
+// They don't use any bit math or unsigned ints, so should be compatible with GLSL ES 2.0.
+// The hex constants are the top 32 bits of the constants for the R4 sequence by Martin Roberts.
+// The ivec of constants has the same bits as uvec4(0xDB4F0B91u, 0xBBE05633u, 0xA0F2EC75u, 0x89E18285u), which may
+// be more useful when uvec4 is available.
+
+int triffidhash1(int p)
+{
+    ivec4 v = p * p + (ivec4(-615576687, -1142925773, -1594692491, -1981709691));
+    v = (v * v + v) / ivec4(5, 3, 2, 1);
+    return v.x + v.y + v.z + v.w;
+}
+
+int triffidhash2(ivec2 p)
+{
+    ivec4 v = p.xxyy * p.yxxy + (p.xyyx * ivec4(-615576687, -1142925773, -1594692491, -1981709691));
+    v = (v * v + v) / ivec4(5, 3, 2, 1);
+    return v.x + v.y + v.z + v.w;
+}
+
+int triffidhash3(ivec3 p)
+{
+    ivec4 v = p.xyzz * p.yzxy + (p.zxyx * ivec4(-615576687, -1142925773, -1594692491, -1981709691));
+    v = (v * v + v) / ivec4(5, 3, 2, 1);
+    return v.x + v.y + v.z + v.w;
+}
+
+int triffidhash4(ivec4 p)
+{
+    ivec4 v = p.xyzw * p.yzwx + (p.zwxy * ivec4(-615576687, -1142925773, -1594692491, -1981709691));
+    v = (v * v + v) / ivec4(5, 3, 2, 1);
+    return v.x + v.y + v.z + v.w;
 }
