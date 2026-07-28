16b4299a3a2857f7840b88e10defd84a9ffd794e
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-07-27 21:38:28 -0700

    Sanaradj could really use more doc comments!
    It's pretty opaque without them.

--- assets/sanaradj_fragment.glsl
+++ assets/sanaradj_fragment.glsl
@@ -56,36 +56,38 @@ float cosmic(float seed, vec4 con)
 //}
 
 void main() {
-  if(texture2D(u_texture, v_texCoords).a <= 0.) discard;
-  float DIVISIONS = mod(floor(u_seed), 10.0) + 2.0;
-  float TWISTINESS = sin(PI2 * u_adj.a) * 5.0 + 6.0;
+    if (texture2D(u_texture, v_texCoords).a <= 0.) discard;
+    float DIVISIONS = mod(floor(u_seed), 10.0) + 2.0;
+    float TWISTINESS = sin(PI2 * u_adj.a) * 5.0 + 6.0;
 
-    // Normalized pixel coordinates (from 0 to 1)
-  vec2 center = (gl_FragCoord.xy - 0.5 * u_resolution.xy)/u_resolution.y * SCALE;
-  float c = u_time;
-  float len = length(center);
-  float theta = atan(center.y, center.x) * DIVISIONS + c;
-  float shrunk = len * (0.375 * POINTINESS / DIVISIONS);
-  float adj = len * PI2 * 0.75 - c;
-  vec2 i = vec2(theta + len * 5., adj);
-
-    vec4 v = vec4(sin(i.x) * shrunk, cos(i.x) * shrunk, sin(i.y), cos(i.y));
+    // Normalized pixel coordinates (from SCALE times -0.5 to 0.5 on y, typically less on x)
+    vec2 center = (gl_FragCoord.xy - 0.5 * u_resolution.xy) / u_resolution.y * SCALE;
+    // The magnitude polar coordinate; how far the fragment we are rendering is from the center of the screen
+    float len = length(center);
+    // The angle from the center of the screen to the fragment we are rendering, times divisions, and rotating over time
+    float theta = atan(center.y, center.x) * DIVISIONS + u_time;
+    // With more divisions, we need a smaller length to avoid making changing areas too dense
+    float shrunk = len * (0.375 * POINTINESS / DIVISIONS);
+    // Time-adjusted angles; we call sin and cos on these.
+    vec2 rel = vec2(theta + len * 5., len * PI2 * 0.75 - u_time);
+    // No good name for this; it's sin and cos called on rel's angles, with the first two shrunk down based on divisions
+    vec4 v = vec4(sin(rel.x) * shrunk, cos(rel.x) * shrunk, sin(rel.y), cos(rel.y));
+    // A big part of the chaotic appearance comes from this.
+    // All of the constants should be different, otherwise they don't really matter.
     vec4 s = vec4(sin(v.x - 1.11 + TWISTINESS * cos(v.x - 5.3157)),
                   sin(v.y + 1.41 + TWISTINESS * cos(v.y + 4.8142)),
                   sin(v.z + 2.61 + TWISTINESS * cos(v.z - 3.5190)),
                   sin(v.w - 2.31 + TWISTINESS * cos(v.w + 9.1984))) * 1.5;
-//    float aTime = c * 0.1 - len;
-//    vec4 s = vec4(swayRandomized(-16405.3157, aTime - 1.11),
-//                  swayRandomized(-77664.8142, aTime + 1.41),
-//                  swayRandomized(-50993.5190, aTime + 2.61),
-//                  swayRandomized(-42069.1984, aTime - 2.31)) * 1.5 * (vec4(sin(i.xy), cos(i.xy)) + 1.25);
-//    vec4 con = vec4(0.0004375, 0.0005625, 0.0008125, 0.000625) + s;
+    // Our g and b uniforms, taken from the 0-1 range to the 0-PI2 range so we can call sin and cos on them.
     vec2 angles = (u_adj.gb * PI2);
+    // This incorporates everything so far except the seed at first, and then uses it too.
     vec4 con = vec4(0.4375, 0.5625, 0.8125, 0.625) + s + vec4(sin(angles), cos(angles)) * 4.0;
     con.x = cosmic(u_seed, con);
     con.y = cosmic(u_seed, con);
     con.z = cosmic(u_seed, con);
 
+    // Gets con into a 0-1 range.
     con.xyz = sin((con.xyz) * 3.14159265) * 0.5 + 0.5;
+    // Hue-rotates by the r uniform, if non-0, and sets alpha to 1, then tints by u_color.
     gl_FragColor = vec4(applyHue(con.xyz, u_adj.r), 1.0) * v_color;
 }
