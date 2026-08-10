0394c5e7cf3742065cb56d0da94e4233089408a9
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-08-09 21:00:10 -0700

    This grayscale shader looks nice!

--- core/src/main/java/com/github/tommyettinger/sonorant/EndlessShaderNoise.java
+++ core/src/main/java/com/github/tommyettinger/sonorant/EndlessShaderNoise.java
@@ -27,7 +27,7 @@ public class EndlessShaderNoise extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture pixel;
     private int shaderIndex = 0;
-    private final ShaderProgram[] shaders = new ShaderProgram[9];
+    private final ShaderProgram[] shaders = new ShaderProgram[10];
 
     private long startTime;
     private float seed = 3.1337f;
@@ -121,6 +121,14 @@ public class EndlessShaderNoise extends ApplicationAdapter {
         shaders[8] = shaderBananarAdj = new ShaderProgram(Gdx.files.internal("foam_vertex.glsl"), Gdx.files.internal("bananaradj_fragment.glsl"));
         if (!shaderBananarAdj.isCompiled()) {
             Gdx.app.error("Shader", "error compiling shaderBananarAdj:\n" + shaderBananarAdj.getLog());
+            Gdx.app.exit();
+            return;
+        }
+
+        ShaderProgram shaderGrAdj;
+        shaders[9] = shaderGrAdj = new ShaderProgram(Gdx.files.internal("foam_vertex.glsl"), Gdx.files.internal("gradj_fragment.glsl"));
+        if (!shaderGrAdj.isCompiled()) {
+            Gdx.app.error("Shader", "error compiling shaderGrAdj:\n" + shaderGrAdj.getLog());
             Gdx.app.exit();
             return;
         }
