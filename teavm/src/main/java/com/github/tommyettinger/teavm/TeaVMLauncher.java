2209a7d04668b55d9716811c79f8cecd0711c276
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-09-13 01:54:48 -0700

    Update builds to have valtue noise.

--- teavm/src/main/java/com/github/tommyettinger/teavm/TeaVMLauncher.java
+++ teavm/src/main/java/com/github/tommyettinger/teavm/TeaVMLauncher.java
@@ -15,6 +15,6 @@ public class TeaVMLauncher {
 //        new WebApplication(new ShaderNoise(new ReadWriteClipboard(), System.currentTimeMillis()), config);
 //        config.width = 750;
 //        config.height = 1000;
-        new WebApplication(new com.github.tommyettinger.sonorant.ShaderNoise(new ReadWriteClipboard(), System.currentTimeMillis()), config);
+        new WebApplication(new com.github.tommyettinger.sonorant.InputShaderNoise(new ReadWriteClipboard(), System.currentTimeMillis()), config);
     }
 }
